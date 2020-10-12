24de7972cd3ebb6304be28b142ceb5d4964e9898
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-12 16:11:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,11 @@
 def reverse_each_word(string)
   array = []
   array << string.split
+  array.each do |value|
+    value.reverse!
+  end
+  array
+end
 
 def reverse_each_word(string)
   string.split.collect {|word| word.reverse}.join(" ")
